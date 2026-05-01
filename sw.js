ef049289a4f5aec91bdf9498b11cd4f752f1afe2
BG-user-OK <GitHub@bogdangawel.pl>
2026-05-01 11:25:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker dla Tarczy czasu pracy
 // WAŻNE: przy każdej nowej wersji aplikacji zmień nazwę cache - stare wersje automatycznie się usuną.
-const CACHE_NAME = 'tarcza-v1.24';
+const CACHE_NAME = 'tarcza-v1.25';
 
 const FILES_TO_CACHE = [
   './',
